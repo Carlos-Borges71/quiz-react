3e30314cda7ea45b0e069810515ad85087cb694c
Carlos Borges <kaca_master@hotmail.com>
2025-01-11 23:20:34 -0300

    Criando o contexto

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
+import { QuizContext } from './context/quiz';
+
+
 import Quiz from '../img/quiz.svg'
 
 import './Welcome.css';
 
 function Welcome() {
+  const quizState = useContext(QuizContext)
+
+  console.log(quizState)
+
   return (
-    <div className='welcome'>
+    <div className="welcome">
         <h2>Sejam bem-vindo</h2>
         <p>clique no boão abaixo para começar:</p>
         <button>Iniciar</button>
